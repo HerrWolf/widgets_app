da80100af1fcc351dcdcb0eb327b29033263bd73
HerrWolf <webmasterp2m@gmail.com>
2024-03-07 12:08:20 -0800

    seccion 9 clase 4

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink,
+  Colors.pinkAccent,
+  Colors.amber,
+  Colors.brown,
+  Colors.cyan,
+  Colors.indigo,
+  Colors.lime,
+  Colors.deepOrange,
+  Colors.lightBlue,
+  Colors.lightGreen,
+  Colors.yellow,
+  Colors.grey,
+  Colors.blueGrey,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }):assert(selectedColor >= 0, 'selectedColor must be greater than 0'),
+  assert(selectedColor < colorList.length, 'selectedColor must be less than ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor],
+  );
+}
