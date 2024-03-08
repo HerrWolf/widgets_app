7762bdddd206e44ae67a047307e423f5112c8c22
HerrWolf <webmasterp2m@gmail.com>
2024-03-07 22:25:33 -0800

    seccion 9 clase 5

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -34,5 +34,8 @@ class AppTheme {
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
     colorSchemeSeed: colorList[selectedColor],
+    appBarTheme: const AppBarTheme(
+      centerTitle: false,
+    ),
   );
 }
