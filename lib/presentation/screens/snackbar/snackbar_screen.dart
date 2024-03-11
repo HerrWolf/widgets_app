81b8c4c09f7536ef5612c2d1dedcbc6c217132d0
HerrWolf <webmasterp2m@gmail.com>
2024-03-11 14:45:45 -0700

    seccion 10 clase 7

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
 
@@ -19,11 +20,52 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      // esta opcion obliga al usuario a tomar una decision antes de continuar
+      barrierDismissible: false,
+      context: context, 
+      builder: (context) => AlertDialog(
+        title: const Text('¿Estas seguro?'),
+        content: const Text('Esse esse duis est consectetur ad. Cupidatat irure pariatur magna eiusmod esse non pariatur. Ad tempor sit mollit dolore enim magna irure laborum Lorem laborum aute do. Sit nulla dolor in dolor. Sit id amet est amet magna pariatur velit ad excepteur.'),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(), 
+            child: const Text('Cancelar')
+          ),
+          FilledButton(onPressed: () => context.pop(), child: const Text('Aceptar'))
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbars y Diálogos'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text('Officia amet amet qui exercitation. Officia ea consectetur amet proident nostrud. Laborum anim sit esse laboris irure adipisicing dolore commodo pariatur do consequat sit. Sunt reprehenderit ex irure exercitation magna nostrud excepteur amet. Consectetur cupidatat voluptate non eu. Est cupidatat excepteur est qui veniam aute nisi excepteur nisi ea.'),
+                  ],
+                );
+              },
+              child: const Text('Licencias usadas'),
+            ),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: const Text('Mostrar diálogo'),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Mostrar Snackbar'),
