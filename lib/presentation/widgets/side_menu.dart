165db29d7dc09f4af2272a4ebbf77136c91efe2e
HerrWolf <webmasterp2m@gmail.com>
2024-03-16 13:25:11 -0700

    seccion 11 clase 4

---
+++ lib/presentation/widgets/side_menu.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SideMenu extends StatefulWidget {
+  const SideMenu({super.key});
+
+  @override
+  State<SideMenu> createState() => _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu> {
+
+  int navDrawerIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationDrawer(
+      selectedIndex: navDrawerIndex,
+      onDestinationSelected: (value) {
+        setState(() {
+          navDrawerIndex = value;
+        });
+      },
+      children: const [
+        Padding(
+          padding: EdgeInsets.fromLTRB(28, 30, 16, 10),
+          child: Text('Main Menu')
+        ),
+        NavigationDrawerDestination(
+          icon: Icon(Icons.add), 
+          label: Text('Home Screen'),
+        ),
+        NavigationDrawerDestination(
+          icon: Icon(Icons.add_shopping_cart_rounded), 
+          label: Text('Otra pantalla'),
+        ),
+      ],
+    );
+  }
+}
